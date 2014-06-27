674ee804559c11a9e8c8c533cccaa2e53d011e5a
david <dagaydevel@sourceforge.net>
2014-06-27 23:34:50 +0200

    extracting poly.lua file

--- main.lua
+++ main.lua
@@ -1,101 +1,7 @@
 
-PolyMetaTable = {}
-PolyMetaTable.__index = PolyMetaTable;
+new_poly = require "poly"
 
-function PolyMetaTable.is_closed(poly)
-  local nb = #poly
-  return nb >=6 and poly[1] == poly[nb-1] and poly[2] == poly[nb]
-end
-
-function PolyMetaTable.is_convex(poly)
-  if not poly:is_closed() then
-    return false
-  end
-
-  local zsign = 0
-  for i = 1,(#poly-2),2 do
-    local n = i
-    local x1, y1 = poly[n], poly[n+1]
-    n = n + 2 < #poly and n + 2 or 3
-    local x2, y2 = poly[n], poly[n+1]
-    n = n + 2 < #poly and n + 2 or 3
-    local x3, y3 = poly[n], poly[n+1]
-    -- dot product of the z component
-    local z = (x1 - x2) * (y3 - y2) - (y1 - y2) * (x3 - x2)
-
-    if zsign == 0 then
-      zsign = z
-    elseif zsign * z < 0 then
-      return false
-    end
-  end
-  return true
-end
-
-function PolyMetaTable.get_coord_count(poly)
-  return #poly / 2
-end
-
-function PolyMetaTable.get_coord(poly, index)
-  return poly[index*2-1], poly[index*2]
-end
-
-function PolyMetaTable.push_coord(poly, x, y)
-  table.insert(poly, x)
-  table.insert(poly, y)
-end
-
-function PolyMetaTable.pop_coord(poly)
-  if #poly > 1 then
-    table.remove(poly)
-    table.remove(poly)
-  end
-end
-
-function PolyMetaTable.close(poly)
-  if #poly >= 6 and not poly:is_closed() then
-    table.insert(poly, poly[1])
-    table.insert(poly, poly[2])
-  end
-end
-
-local poly = setmetatable({}, PolyMetaTable)
-
-if not love then
-  love = {}
-end
-
-function findConvex(poly)
-  local convex = {poly[1], poly[2]}
-  local nbSection = 1
-
-  local zsign = 0
-  for i = 1,(#poly-2),2 do
-    local n = i
-    local x1, y1 = convex[#convex-1], convex[#convex]
-    n = n + 2 < #poly and n + 2 or 3
-    local x2, y2 = poly[n], poly[n+1]
-    n = n + 2 < #poly and n + 2 or 3
-    local x3, y3 = poly[n], poly[n+1]
-    -- dot product of the z component
-    local z = (x1 - x2) * (y3 - y2) - (y1 - y2) * (x3 - x2)
-
-    if zsign == 0 then
-      zsign = z
-    end
-    if zsign * z >= 0 then
-      table.insert(convex, x2)
-      table.insert(convex, y2)
-    else
-      nbSection = nbSection + 1
-      if nbSection > 2 then
-        break
-      end
-      break
-    end
-  end
-  return convex
-end
+local poly = new_poly()
 
 function love.load(args)
   love.graphics.setBackgroundColor(250,250,250)
@@ -135,10 +41,6 @@ function love.draw()
   elseif nb_coord == 1 then
     love.graphics.point(poly:get_coord(1))
   end
---[[  
-  if poly:is_closed() then
-    love.graphics.polygon('fill', findConvex(poly))
-  end
---]]
+
   love.graphics.print(msg, 10, 10)
 end
---
+++ poly.lua
@@ -0,0 +1,64 @@
+local PolyMetaTable = {}
+PolyMetaTable.__index = PolyMetaTable;
+
+function PolyMetaTable.is_closed(poly)
+  local nb = #poly
+  return nb >=6 and poly[1] == poly[nb-1] and poly[2] == poly[nb]
+end
+
+-- I know löve already provides this method
+function PolyMetaTable.is_convex(poly)
+  if not poly:is_closed() then
+    return false
+  end
+
+  local zsign = 0
+  for i = 1,(#poly-2),2 do
+    local n = i
+    local x1, y1 = poly[n], poly[n+1]
+    n = n + 2 < #poly and n + 2 or 3
+    local x2, y2 = poly[n], poly[n+1]
+    n = n + 2 < #poly and n + 2 or 3
+    local x3, y3 = poly[n], poly[n+1]
+    -- dot product of the z component
+    local z = (x1 - x2) * (y3 - y2) - (y1 - y2) * (x3 - x2)
+
+    if zsign == 0 then
+      zsign = z
+    elseif zsign * z < 0 then
+      return false
+    end
+  end
+  return true
+end
+
+function PolyMetaTable.get_coord_count(poly)
+  return #poly / 2
+end
+
+function PolyMetaTable.get_coord(poly, index)
+  return poly[index*2-1], poly[index*2]
+end
+
+function PolyMetaTable.push_coord(poly, x, y)
+  table.insert(poly, x)
+  table.insert(poly, y)
+end
+
+function PolyMetaTable.pop_coord(poly)
+  if #poly > 1 then
+    table.remove(poly)
+    table.remove(poly)
+  end
+end
+
+function PolyMetaTable.close(poly)
+  if #poly >= 6 and not poly:is_closed() then
+    table.insert(poly, poly[1])
+    table.insert(poly, poly[2])
+  end
+end
+
+return function()
+  return setmetatable({}, PolyMetaTable)
+end
